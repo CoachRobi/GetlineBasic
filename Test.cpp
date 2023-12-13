3273de22132f2f60c10dd887c7159eb1e1028740
coach_robi <robideauj@gm.fsd38.ab.ca>
2023-12-13 13:09:55 -0700

    Add project files.

---
+++ Test.cpp
@@ -0,0 +1,30 @@
+// Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "What is the first two planets in our solar system? Just use a space between and no caps\n";
+    std::string input;
+    std::getline(std::cin, input);
+    
+
+    if (input == "mercury venus") 
+        std::cout << "You are correct!\n";
+    
+    else
+        std::cout << "Sorry, that is not the correct response.\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
